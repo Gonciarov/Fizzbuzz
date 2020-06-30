88687267138f278717c41cfbf4b930160facd356
gonciarov <gonciarov@yandex.ru>
2020-06-30 14:39:03 +0100

    Added further tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,3 +5,15 @@ describe 'fizzbuzz.rb' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
 end
+
+describe 'fizzbuzz.rb' do
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+end
+
+describe 'fizzbuzz.rb' do
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+end
